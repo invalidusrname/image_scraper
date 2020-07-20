745e2cc7db7df155a44832e9789b5c5a347ae3e6
Matt McMahand <matt@invalid8.com>
2020-07-20 13:37:55 -0400

    update gemspec

--- lib/image_scraper.rb
+++ lib/image_scraper.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+module ImageScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
 require 'rails'
 require 'open-uri'
 require 'nokogiri'
@@ -7,3 +12,4 @@ require 'nokogiri'
 require 'image_scraper/railtie'
 require 'image_scraper/util'
 require 'image_scraper/client'
+require "image_scraper/version"
---
+++ lib/image_scraper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ImageScraper
+  VERSION = '0.1.8'
+end
